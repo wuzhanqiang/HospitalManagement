0f685f782132c5766a467f5e3df8316d94d93754
wuzhanqiang <904898369@qq.com>
2019-05-08 02:03:16 +0800

    新建工程

---
+++ HospitalManagement/src/com/demo/service/impl/TestServiceImpl.java
@@ -0,0 +1,9 @@
+package com.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.demo.service.TestService;
+
+@Service
+public class TestServiceImpl implements TestService {
+	
+}
